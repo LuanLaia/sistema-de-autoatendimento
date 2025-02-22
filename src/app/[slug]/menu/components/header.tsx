838c387d8c9ee8831b28efdba07a6f30c2858378
Luan Laia <luamperreira@gmail.com>
2025-02-22 19:08:09 -0300

    feat: add initial restaurant page and menu page

---
+++ src/app/[slug]/menu/components/header.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import { Restaurant } from "@prisma/client";
+import { ChevronLeftIcon, ScrollTextIcon } from "lucide-react";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+
+import { Button } from "@/components/ui/button";
+
+interface RestaurantHeaderProps {
+  restaurant: Pick<Restaurant, "coverImageUrl" | "name">;
+}
+
+const RestaurantHeader = ({ restaurant }: RestaurantHeaderProps) => {
+  const router = useRouter();
+  const handleBlackClick = () => router.back()
+  return (
+    <div>
+      <div className="relative h-[250px] w-full">
+        <Button
+          variant="secondary"
+          size="icon"
+          className="absolute left-4 top-4 z-50 rounded-full"
+          onClick={handleBlackClick}
+        >
+          <ChevronLeftIcon />
+        </Button>
+        <Image
+          src="/coverImageUrl.png"
+          alt={restaurant.name}
+          className="object-cover"
+          fill
+        />
+        <Button
+          variant="secondary"
+          size="icon"
+          className="absolute right-4 top-4 z-50 rounded-full"
+        >
+          <ScrollTextIcon />
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default RestaurantHeader;
