617ece1e31ae233a04d865644fbea942b75ffc27
Luan Laia <luamperreira@gmail.com>
2025-02-23 23:14:39 -0300

    feat: add initial add to cart function to cart context

--- src/app/[slug]/menu/[productId]/components/products-details.tsx
+++ src/app/[slug]/menu/[productId]/components/products-details.tsx
@@ -16,7 +16,7 @@ interface ProductDetailsProps {
 }
 
 const ProductDetails = ({product}: ProductDetailsProps) => {
-    const {toggleCart} = useContext(CartContext);
+    const {toggleCart, addProduct} = useContext(CartContext);
     const [quantity, setQuantity] = useState<number>(1)
     const handleDecreaseQuantity = () => {
         setQuantity((prev) => {
@@ -30,6 +30,10 @@ const ProductDetails = ({product}: ProductDetailsProps) => {
         setQuantity((prev) => prev + 1);
     }
     const handleAddToCart = () => {
+        addProduct({
+            ...product,
+            quantity,
+        })
         toggleCart();
     };
     return (  
