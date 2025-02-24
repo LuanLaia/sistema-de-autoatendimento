a44d7f49ff022b5cb0046c78b9721dadbb492c9a
Luan Laia <luamperreira@gmail.com>
2025-02-24 13:53:23 -0300

    feat: ensure cart product quantity can be increased

--- src/app/[slug]/menu/context/cart.tsx
+++ src/app/[slug]/menu/context/cart.tsx
@@ -12,7 +12,8 @@ export interface ICartContext {
     products: CartProduct[],
     toggleCart: () => void,
     addProduct: (product: CartProduct) => void;
-    decreaseProductQuantity: (productId: string) => void
+    decreaseProductQuantity: (productId: string) => void;
+    increaseProductQuantity: (productId: string) => void;
 }
 
 export const CartContext = createContext<ICartContext>({
@@ -21,6 +22,7 @@ export const CartContext = createContext<ICartContext>({
     toggleCart: () => {},
     addProduct: () => {},
     decreaseProductQuantity: () => {},
+    increaseProductQuantity: () => {},
 })
 
 export const CartProvider = ({children}: {children: ReactNode}) => {
@@ -64,6 +66,17 @@ export const CartProvider = ({children}: {children: ReactNode}) => {
             })
         })
     }
+
+    const increaseProductQuantity = (productId: string) => {
+        setProducts((prevProducts) => {
+            return prevProducts.map((prevProduct) => {
+                if (prevProduct.id != productId) {
+                    return prevProduct;
+                }
+                return {...prevProduct, quantity: prevProduct.quantity + 1}
+            })
+        })
+    }
     return (
         <CartContext.Provider value={{
             isOpen,
@@ -71,6 +84,7 @@ export const CartProvider = ({children}: {children: ReactNode}) => {
             toggleCart,
             addProduct,
             decreaseProductQuantity,
+            increaseProductQuantity,
         }}>
             {children}
         </CartContext.Provider>
