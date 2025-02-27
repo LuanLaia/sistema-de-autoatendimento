e7984453945b0d5faa395b776437bf94e63faf82
Luan Laia <luamperreira@gmail.com>
2025-02-27 02:25:33 -0300

    feat: ensure cart summary is shown on restaurant page

--- src/app/[slug]/menu/components/categories.tsx
+++ src/app/[slug]/menu/components/categories.tsx
@@ -3,11 +3,14 @@ import { Prisma } from "@prisma/client";
 import { ScrollArea } from "@radix-ui/react-scroll-area";
 import { ClockIcon } from "lucide-react";
 import Image from "next/image";
-import { useState } from "react";
+import { useContext, useState } from "react";
 
 import { Button } from "@/components/ui/button";
 import { ScrollBar } from "@/components/ui/scroll-area";
+import { formatCurrency } from "@/helpers/format-currency";
 
+import CartSheet from "../[productId]/components/cart-sheet";
+import { CartContext } from "../context/cart";
 import Products from "./products";
 
 interface RestaurantCategoriesProps {
@@ -30,6 +33,7 @@ const RestaurantCategories = ({restaurant}: RestaurantCategoriesProps) => {
     const handleCategoryClick = (category: MenuCategoriesWithProducts) => {
         setSelectCategory(category)
     };
+    const {products,totalQuantity,total, toggleCart} = useContext(CartContext);
 
     const getCategoryButtonVariant = (category: MenuCategoriesWithProducts) => {
        return selectedCategory.id == category.id ? "default" : "secondary"
@@ -69,6 +73,16 @@ const RestaurantCategories = ({restaurant}: RestaurantCategoriesProps) => {
 
             <h3 className="px-5 pt-2 font-semibold">{selectedCategory.name}</h3>
             <Products products={selectedCategory.products} />
+            {products.length > 0 && (
+                <div className="fixed bottom-0 left-0 right-0 flex w-full items-center justify-between bg-white px-5 py-3 border-t">
+                    <div>
+                        <p className="text-xs text-muted-foreground">Total dos pedidos</p>
+                        <p className="text-sm font-semibold">{formatCurrency(total)}<span className="text-xs font-normal text-muted-foreground">/ {totalQuantity} {totalQuantity > 1 ? "itens" : "item"} </span></p>
+                    </div>
+                    <Button onClick={toggleCart}>Ver sacola</Button>
+                    <CartSheet/>
+                </div>
+            )}
         </div>
     );
 }
